64965548b2e15dbf3894383a276036a73112883f
Michaeleduardoo <michaeledu20@outlook.com>
2023-07-24 19:11:29 -0300

    Scroll

---
+++ scripts.js
@@ -0,0 +1,11 @@
+const myObsever = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+    } else entry.target.classList.remove("show");
+  });
+});
+
+const elements = document.querySelectorAll(".hidden");
+
+elements.forEach((element) => myObsever.observe(element));
